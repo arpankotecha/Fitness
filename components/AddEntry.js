2e550d66737710179c04be0e35e17a5b5806cb61
Arpan Kotecha <arpankotecha@gmail.com>
2017-12-01 06:15:56 -0500

    Make entries in the history clickable and resetable.

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -10,6 +10,7 @@ import { submitEntry, removeEntry } from '../utils/api'
 import { connect } from 'react-redux'
 import { addEntry } from '../actions'
 import { white, purple } from '../utils/colors'
+import { NavigationActions } from 'react-navigation'
 
 function SubmitBtn ({ onPress }) {
   return (
@@ -75,7 +76,9 @@ class AddEntry extends Component {
       sleep: 0,
       eat: 0
     }))
-    // Navigate to home
+
+    this.toHome()
+
     submitEntry({ key, entry })
     // Clear local notification
   }
@@ -86,9 +89,16 @@ class AddEntry extends Component {
     this.props.dispatch(addEntry({
       [key]: getDailyReminderValue()
     }))
-    //Route to Home
-    //
+
+    this.toHome()
+
     removeEntry(key)
+  }
+
+  toHome = () => {
+    this.props.navigation.dispatch(NavigationActions.back({
+      key: `AddEntry`
+    }))
   }
 
   render() {
